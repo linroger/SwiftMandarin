0623532665164b15b1d11961e2783949352262f4
Roger Lin <roger.lin@scholar.app>
2026-02-11 01:16:09 +0800

    Initial Commit

---
+++ SwiftMandarin/SwiftMandarinApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftMandarinApp.swift
+//  SwiftMandarin
+//
+//  Created by Roger Lin on 2/11/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftMandarinApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
